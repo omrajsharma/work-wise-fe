eb0070ff648be7fcb422db7682fe24d1473435c9
omrajsharma <omraj7413@gmail.com>
2023-07-25 21:50:21 +0530

    feat: app header

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Logo from '../../assets/owl-transparent.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="header-container">
+        <div className="header-left">
+          <Link to='/' className="header-brand">
+            <img src={Logo} alt="" />
+            <h2>Work Wise</h2>
+          </Link>
+        </div>
+        <div className="header-right">
+          <div className="header-login">
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
