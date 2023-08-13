cb50f901b40e19d4a495f664e6d53e0ac0d79372
omrajsharma <omraj7413@gmail.com>
2023-08-13 10:16:40 +0530

    feat: user login

--- src/pages/LoginUserPage.jsx
+++ src/pages/LoginUserPage.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { successAlert, errorAlert } from '../utils/alert';
 
 function LoginUserPage() {
+  const username = React.useRef();
+  const password = React.useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const usernameVal = username.current.value;
+    const passwordVal = password.current.value;
+
+    const response = await fetch('http://localhost:3000/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: usernameVal,
+        password: passwordVal
+      }),
+      credentials: "include",
+    })
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      successAlert(data.success);
+    } else {
+      errorAlert(data.error);
+    }
+  }
+
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login-container">
+        <div className="login-form">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="text" className='input-field' placeholder='User Name' ref={username} required/>
+            <input type="password" className='input-field' placeholder='Password' ref={password} required/>
+            <button>Sign Up</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
