7efe67420e163daa9c5a54c60d0b606d4b371036
omrajsharma <omraj7413@gmail.com>
2023-08-16 22:20:03 +0530

    feat: profile check

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from '../../assets/owl-transparent.png'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../../context/UserContextProvider'
 
 function Header() {
   const {userInfo, setUserInfo} = React.useContext(UserContext);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/profile`, {
+      credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => setUserInfo(data.data))
+  }, [])
 
   return (
     <header>
